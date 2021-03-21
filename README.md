18e3602f3175bb42df9c7cc5b97475ae4757f65c
dirtyshow <showbowlee@gmail.com>
2021-03-21 14:01:37 +0800

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# 金书红颜录chm build
+# 金書紅顏錄
 
 http://tpr.inkit.org/
 
 使用MS chm Workshop打包。
+
+遊戲版區: http://game.ali213.net/forum-360-1.html
---
+++ js/main.js
@@ -0,0 +1,5 @@
+﻿function GetHtmlContent(component_id, page) {
+    $.get(page, function (data) {
+        $('#' + component_id).html(data);
+    });
+}
